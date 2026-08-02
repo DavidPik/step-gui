30e56ba25217606d9e00cb49c7d98d3914c09103
David Pikálek <123298047+DavidPik@users.noreply.github.com>
2026-08-02 16:00:20 +0200

    Add authorities page to display authority list

---
+++ frontend/src/routes/authorities/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { apiGet } from "$lib/api";
+
+    let authorities = [];
+
+    apiGet("authorities").then(data => {
+        authorities = data;
+    });
+</script>
+
+<h1>Authorities</h1>
+
+{#if authorities.length === 0}
+    <p>No authorities found.</p>
+{:else}
+    <ul>
+        {#each authorities as a}
+            <li>{a.name} ({a.type})</li>
+        {/each}
+    </ul>
+{/if}
