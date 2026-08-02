feeab6019496cc6df1d910d56a4ddc216f9da6dd
David Pikálek <123298047+DavidPik@users.noreply.github.com>
2026-08-02 16:01:42 +0200

    Add provisioners page to display provisioner list

---
+++ frontend/src/routes/provisioners/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { apiGet } from "$lib/api";
+
+    let provisioners = [];
+
+    apiGet("provisioners").then(data => {
+        provisioners = data;
+    });
+</script>
+
+<h1>Provisioners</h1>
+
+{#if provisioners.length === 0}
+    <p>No provisioners found.</p>
+{:else}
+    <ul>
+        {#each provisioners as p}
+            <li>{p.name} ({p.type})</li>
+        {/each}
+    </ul>
+{/if}
