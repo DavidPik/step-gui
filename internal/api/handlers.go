f3adeb933e28c2ec8e1b5dd8332b4a5bfb233958
David Pikálek <123298047+DavidPik@users.noreply.github.com>
2026-08-02 15:49:23 +0200

    Add initial handler functions for API

---
+++ internal/api/handlers.go
@@ -0,0 +1,27 @@
+package handlers
+
+import "github.com/labstack/echo/v4"
+
+func RegisterAuthorityHandlers(g *echo.Group) {
+    // TODO
+}
+
+func RegisterPolicyHandlers(g *echo.Group) {
+    // TODO
+}
+
+func RegisterProvisionerHandlers(g *echo.Group) {
+    // TODO
+}
+
+func RegisterCertificateHandlers(g *echo.Group) {
+    // TODO
+}
+
+func RegisterUserHandlers(g *echo.Group) {
+    // TODO
+}
+
+func RegisterAuditHandlers(g *echo.Group) {
+    // TODO
+}
