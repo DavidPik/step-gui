4fab1bbbad28e60e957bd684e855d70e13a2bbb2
David Pikálek <123298047+DavidPik@users.noreply.github.com>
2026-08-02 15:45:15 +0200

    Implement error handling for config loading
    Add error handling for config loading

--- cmd/gui_backend/main.go
+++ cmd/gui_backend/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+    "log"
     "step-gui/internal/config"
     "step-gui/internal/server"
 )
 
 func main() {
-    cfg := config.Load()
+    cfg, err := config.Load()
+    if err != nil {
+            log.Fatalf("failed to load config: %v", err)
+    }
+    
     server.Start(cfg)
 }
