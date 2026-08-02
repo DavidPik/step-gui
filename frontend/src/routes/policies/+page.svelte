e6a9258497933126c86aea7dc9830445070d68b3
David Pikálek <123298047+DavidPik@users.noreply.github.com>
2026-08-02 16:00:56 +0200

    Add policies page to display fetched policies

---
+++ frontend/src/routes/policies/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { apiGet } from "$lib/api";
+
+    let policies = [];
+
+    apiGet("policies").then(data => {
+        policies = data;
+    });
+</script>
+
+<h1>Policies</h1>
+
+{#if policies.length === 0}
+    <p>No policies found.</p>
+{:else}
+    <ul>
+        {#each policies as p}
+            <li>{p.name} (v{p.version})</li>
+        {/each}
+    </ul>
+{/if}
