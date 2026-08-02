208f135e0e6ab8363eb0c19504507f9615e76403
David Pikálek <123298047+DavidPik@users.noreply.github.com>
2026-08-02 15:51:53 +0200

    Format New function for better readability

--- internal/stepca/client.go
+++ internal/stepca/client.go
@@ -7,5 +7,11 @@ type Client struct {
 }
 
 func New(baseURL, prov string, key []byte) *Client {
-    return &Client{BaseURL: baseURL, Provisioner: prov, Key: key}
+    return &Client{
+        BaseURL:     baseURL,
+        Provisioner: prov,
+        Key:         key,
+    }
 }
+
+// TODO: implement /sign, /revoke, /provisioners (FÁZE 4)
